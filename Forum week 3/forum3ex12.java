83a28428aa3f19f163f251940930d9173ca18388
Jonathan Prasetyo <91464711+Jopras70@users.noreply.github.com>
2022-03-07 22:06:18 +0700

    Add files via upload

---
+++ Forum week 3/forum3ex12.java
@@ -0,0 +1,9 @@
+public class forum3ex12 {
+   public static void main(String[] args) {
+    System.out.println("Number" + "\t" + "Square" + "\t" + "Cube"); 
+
+    for (int x = 0; x <= 10; x++) { 
+        System.out.println(x + "\t" + x*x + "\t" + x*x*x); 
+    }
+   }
+}
